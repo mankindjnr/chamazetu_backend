87c54c809ac26c6aa4d2130fda0bc8e712d4a067
mankindjnr <kamos8646@stu.kemu.ac.ke>
2024-01-28 13:54:48 +0300

    tomorrow we start on dashboard

--- app/router/auth.py
+++ app/router/auth.py
@@ -6,35 +6,53 @@ import logging
 
 from .. import schemas, database, utils, oauth2, models
 
-router = APIRouter(
-    prefix="/users",
-    tags=["Authentication"]
-)
+router = APIRouter(prefix="/users", tags=["Authentication"])
 
-#TODO: Introduce try and catch for all the functions or other error handling methods
+
+# TODO: Introduce try and catch for all the functions or other error handling methods
 @router.post("/login", response_model=schemas.Token)
-async def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
-    
+async def login(
+    user_credentials: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(database.get_db),
+):
+    user = (
+        db.query(models.User)
+        .filter(models.User.email == user_credentials.username)
+        .first()
+    )
+
     if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Credentials"
+        )
 
     if not user.is_active or not user.is_verified:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
-    
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Credentials"
+        )
+
     if not utils.verify_password(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Credentials")
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid Credentials"
+        )
 
     access_token = await oauth2.create_access_token(data={"sub": user.email})
     refresh_token = await oauth2.create_refresh_token(data={"sub": user.email})
-    
-    return {"access_token": access_token, "refresh_token": refresh_token, "token_type": "bearer"}
+
+    return {
+        "access_token": access_token,
+        "refresh_token": refresh_token,
+        "token_type": "bearer",
+    }
+
 
 @router.post("/refresh", response_model=schemas.refreshedToken)
 async def new_access_token(token_data: schemas.TokenData = Body(...)):
     try:
         logging.info(f"token_data: {token_data}")
-        new_access_token = await oauth2.create_access_token(data={"sub": token_data.username})
+        new_access_token = await oauth2.create_access_token(
+            data={"sub": token_data.username}
+        )
         return {"new_access_token": new_access_token, "refreshed_token_type": "bearer"}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
